9386a5093ad08d795cb99721c01a81205c946161
nicolas le flohic <vikking@macbook-pro-de-nicolas.home>
2019-11-13 00:45:59 +0100

    Correct description and name

--- src/Components/Presentation.js
+++ src/Components/Presentation.js
@@ -7,7 +7,7 @@ const Presentation = (props) => {
 			<div className="wrapper wrapper-presentation">
 				<div className="presentation-text">
 					<h1 className="name">{props.restaurant.name}</h1>
-					<p className="description">{props.restaurant.name}</p>
+					<p className="description">{props.restaurant.description}</p>
 				</div>
 				<img className="presentation-image" src={props.restaurant.picture} alt="presentation_picture" />
 			</div>
